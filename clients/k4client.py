6833ea319626be70edda231bb3a3ec93e25efdb3
porpoises <39185647+porpoises@users.noreply.github.com>
2017-05-19 00:49:55 -0700

    first commit

---
+++ clients/k4client.py
@@ -0,0 +1,54 @@
+import functools
+import json
+import socket
+
+
+class RPCError(Exception):
+    pass
+
+
+class ClientError(Exception):
+    pass
+
+
+class MismatchedIDError(Exception):
+    pass
+
+
+class ServerError(Exception):
+    pass
+
+
+class Client(object):
+    def __init__(self):
+        self._id = 0
+        self._f = socket.fromfd(3, socket.AF_UNIX, socket.SOCK_STREAM).makefile(
+            'rwb', buffering=0)
+
+    def call(self, method, **kwargs):
+        req_id = self._id
+
+        self._f.write(json.dumps({
+            'id': req_id,
+            'method': method,
+            'params': [kwargs],
+        }).encode('utf-8'))
+        self._id += 1
+
+        while True:
+            raw = json.loads(self._f.readline())
+
+            resp_id = raw['id']
+            error = raw['error']
+
+            if resp_id < req_id:
+                continue
+
+            if resp_id > req_id:
+                raise MismatchedIDError('expected {}, got {}'.format(
+                    req_id, resp_id))
+
+            if error is not None:
+                raise ServerError(error)
+
+            return raw['result']
