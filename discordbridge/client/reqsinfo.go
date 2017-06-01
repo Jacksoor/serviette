bc3df9e4b2f0f49d26b138ead18f3c82f2cde782
porpoises <39185647+porpoises@users.noreply.github.com>
2017-06-01 01:30:33 -0700

    MORE BOLD

--- discordbridge/client/reqsinfo.go
+++ discordbridge/client/reqsinfo.go
@@ -4,13 +4,13 @@ func explainBillUsageToOwner(c *Client, v bool) string {
 	if v {
 		return "bills usage to the command's owner"
 	} else {
-		return "bills usage to **you**"
+		return "**bills usage to you**"
 	}
 }
 
 func explainNeedsEscrow(c *Client, v bool) string {
 	if v {
-		return "uses the number after the command name as an escrow amount"
+		return "**uses the number after the command name as an escrow amount**"
 	} else {
 		return "does not need to escrow any funds from you"
 	}
