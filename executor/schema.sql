cb2f105523aab31666f7d9d90a6f1c6e0f16b9b5
porpoises <39185647+porpoises@users.noreply.github.com>
2017-06-30 20:15:12 -0700

    add rich to default allowed output formats

--- executor/schema.sql
+++ executor/schema.sql
@@ -5,7 +5,7 @@ create table accounts (
     memory_limit integer not null default 20971520,
     tmpfs_size integer not null default 20971520,
     allow_network_access boolean not null default false,
-    allowed_output_formats character varying[] not null default array['text']::character varying[],
+    allowed_output_formats character varying[] not null default array['text', 'rich']::character varying[],
     allowed_services character varying[] not null default array[]::character varying[]
 );
 
